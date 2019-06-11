7915334b32081ca82f5b3c3c0c8b631608cc4d3e
Rodrigo <ti.rodrigoalmeida@gmail.com>
2019-06-11 03:08:26 -0300

    Salvando o perfil

--- lib/pages/ProfilePage.dart
+++ lib/pages/ProfilePage.dart
@@ -2,124 +2,236 @@ import 'package:estagios/components/default_app_bar.dart';
 import 'package:estagios/connection.dart';
 import 'package:estagios/model/Pessoa.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:masked_text/masked_text.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
+import 'package:validators/validators.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   final Pessoa pessoa;
 
   ProfilePage({Key key, @required this.pessoa}) : super(key: key);
 
   @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  final formKey = GlobalKey<FormState>();
+
+  var _salvando = false;
+  var _deslogando = false;
+
+  final _nome = TextEditingController();
+  final _email = TextEditingController();
+  final _nascimento = MaskedTextController(mask: "00/00/0000");
+  final _telefone = MaskedTextController(mask: "(00) 0000-0000");
+  final _sobre = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    final pessoa = this.widget.pessoa;
+
+    _nome.text = pessoa.nome;
+    _email.text = pessoa.email;
+    _nascimento.text = pessoa.nascimento;
+    _telefone.text = pessoa.telefone1;
+    _sobre.text = pessoa.sobre;
+
+    _telefone.beforeChange = (String previous, String next) {
+      if (next.length >= 15) {
+        _telefone.updateMask("(00) 00000-0000");
+      } else {
+        _telefone.updateMask("(00) 0000-0000");
+      }
+      return true;
+    };
+  }
+
+  @override
   Widget build(BuildContext context) {
     double mediaWidth = MediaQuery.of(context).size.width;
-    final formKey = GlobalKey<FormState>();
-
-    final _nome = TextEditingController(text: pessoa.nome);
-    final _email = TextEditingController(text: pessoa.email);
-    final _nascimento = TextEditingController(text: pessoa.nascimento);
-    final _telefone1 = TextEditingController(text: pessoa.telefone1);
-    final _telefone2 = TextEditingController(text: pessoa.telefone2);
-    final _sobre = TextEditingController(text: pessoa.sobre);
-
     return Scaffold(
       appBar: DefaultAppBar("Perfil"),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: mediaWidth * 0.08),
-        child: Form(
-          key: formKey,
-          child: ListView(
-            children: <Widget>[
-              TextFormField(
-                controller: _nome,
-                decoration: const InputDecoration(
-                  labelText: 'Nome',
-                ),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Campo obrigatório';
-                  }
-                },
-              ),
-              TextFormField(
-                controller: _email,
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                  labelText: 'E-mail',
-                ),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Campo obrigatório';
-                  }
-                },
-              ),
-              TextFormField(
-                controller: _nascimento,
-                keyboardType: TextInputType.datetime,
-                maxLength: 15,
-                decoration: const InputDecoration(
-                  labelText: 'Nascimento',
-                ),
-              ),
-              MaskedTextField(
-                maskedTextFieldController: _telefone1,
-                mask: "(xx) xxxxx-xxxx",
-                maxLength: 15,
-                keyboardType: TextInputType.phone,
-                inputDecoration: new InputDecoration(
-                  labelText: "Telefone",
-                ),
-              ),
-              TextFormField(
-                controller: _sobre,
-                decoration: const InputDecoration(
-                  labelText: 'Sobre',
-                ),
-              ),
-              SizedBox(height: 20.0),
-              Row(
-                children: <Widget>[
-                  RaisedButton(
-                    child: Text('Salvar'),
-                    color: Colors.blue,
-                    textColor: Colors.white,
-                    onPressed: () {
-                      if (formKey.currentState.validate()) {
-                        salvar(context);
-                      }
-                    },
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          SingleChildScrollView(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: <Widget>[
+                Form(
+                  key: formKey,
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                        controller: _nome,
+                        decoration: const InputDecoration(
+                          labelText: 'Nome',
+                        ),
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Campo obrigatório';
+                          }
+                        },
+                      ),
+                      TextFormField(
+                        controller: _email,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(
+                          labelText: 'E-mail',
+                        ),
+                        validator: (value) {
+                          if (value == null || value.trim() == "") {
+                            return null;
+                          }
+                          if (!isEmail(value)) {
+                            return "E-mail inválido";
+                          }
+                        },
+                      ),
+                      TextFormField(
+                        controller: _nascimento,
+                        keyboardType: TextInputType.datetime,
+                        decoration: const InputDecoration(
+                          labelText: 'Nascimento',
+                        ),
+                        validator: (value) {
+                          if (value == null || value.trim() == "") {
+                            return null;
+                          } else if (getData(value) == null) {
+                            return "Data inválida";
+                          }
+                        },
+                      ),
+                      TextFormField(
+                        controller: _telefone,
+                        keyboardType: TextInputType.phone,
+                        decoration: InputDecoration(
+                          labelText: "Telefone",
+                        ),
+                        validator: (value) {
+                          if (value == null || value.trim() == "") {
+                            return null;
+                          }
+                          if (!matches(value,
+                              r"^(\([0-9]{2}\))\s([9]{1})?([0-9]{4})-([0-9]{4})")) {
+                            return "Telefone inválido";
+                          }
+                        },
+                      ),
+                      TextFormField(
+                        controller: _sobre,
+                        decoration: const InputDecoration(
+                          labelText: 'Sobre',
+                        ),
+                      ),
+                      SizedBox(height: 20.0),
+                      Row(
+                        children: <Widget>[
+                          RaisedButton(
+                            child: Text(
+                              _salvando ? 'Salvando...' : 'Salvar',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                              ),
+                            ),
+                            color: Colors.blue,
+                            textColor: Colors.white,
+                            onPressed: () {
+                              if (!_salvando && formKey.currentState.validate()) {
+                                salvar(context);
+                              }
+                            },
+                          ),
+                          SizedBox(width: 10.0),
+                          RaisedButton(
+                            child: Text(
+                              _deslogando ? 'Deslogando...' : 'Deslogar',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                              ),
+                            ),
+                            color: Colors.red[700],
+                            textColor: Colors.white,
+                            onPressed: () {
+                              if (!_deslogando) deslogar(context);
+                            },
+                          ),
+                        ],
+                        mainAxisAlignment: MainAxisAlignment.center,
+                      ),
+                    ],
                   ),
-                  SizedBox(width: 10.0),
-                  RaisedButton(
-                    child: Text('Deslogar'),
-                    color: Colors.red[700],
-                    textColor: Colors.white,
-                    onPressed: () {
-                      deslogar(context);
-                    },
-                  ),
-                ],
-                mainAxisAlignment: MainAxisAlignment.center,
-              ),
-            ],
+                ),
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
 
+  DateTime getData(String str) {
+    try {
+      return DateFormat('dd/MM/yyyy').parse(str);
+    } catch (e) {
+      return null;
+    }
+  }
+
   void salvar(BuildContext context) async {
-    var connection = new Connection();
-    var data = await connection.put("pessoas/${pessoa.id}");
+    setState(() => _salvando = true);
+
+    var params = {
+      'nome': this._nome.text,
+      'email': this._email.text,
+      'nascimento': DateFormat('yyy-MM-dd').format(getData(this._nascimento.text)),
+      'telefone1': this._telefone.text,
+      'sobre': this._sobre.text,
+    };
+
+    var data;
+    try {
+      var connection = new Connection();
+      data = await connection.put(
+          "pessoas/${this.widget.pessoa.id}", params);
+    } finally {
+      setState(() => _salvando = false);
+    }
 
     if (data['success']) {
-      // todo fazer algo
+      await showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Atenção"),
+            content: Text("Perfil salvo com sucesso!"),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("OK"),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          );
+        },
+      );
     }
   }
 
   void deslogar(context) async {
+    setState(() => _deslogando = true);
+
     var connection = new Connection();
     var data = await connection.post('logout');
+
+    setState(() => _deslogando = false);
 
     if (data['success']) {
       SharedPreferences prefs = await SharedPreferences.getInstance();
